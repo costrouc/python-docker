803caaaebf88c91c11371f0fd4ab2197222f5f8c
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-07-31 13:14:25 -0400

    Adding flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "python-docker";
+
+  inputs = {
+    nixpkgs = { url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    devShell.x86_64-linux =
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+        pythonPackages = pkgs.python3Packages;
+      in pkgs.mkShell {
+        buildInputs = [
+          pythonPackages.requests
+        ];
+      };
+  };
+}
